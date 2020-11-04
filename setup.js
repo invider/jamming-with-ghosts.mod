f9b7c0085171d50bc88e08104503ddaef6f0b959
Igor Khotin <khotin@gmx.com>
2020-11-04 22:36:37 +0200

    [*] center the grid

--- setup.js
+++ setup.js
@@ -1,6 +1,8 @@
 function setup() {
 
-    lab.spawn('Grid')
+    const grid = lab.spawn('Grid')
+    grid.x = (rx(1) - grid.w * grid.cw)/2
+    grid.y = (ry(1) - grid.h * grid.ch)/2
 
     for (let [name, ghost] of Object.entries(env.ghosts)) {
         if (name === 'name') continue
